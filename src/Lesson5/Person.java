935cf848a08b5098029ec90b332e94cdc0478cd0
dpano <d.panov27@gmail.com>
2020-02-26 20:15:08 +0300

    Person Class and Object;

---
+++ src/Lesson5/Person.java
@@ -0,0 +1,14 @@
+package Lesson5;
+
+public class Person {
+
+    private String name;
+    private String position;
+    private String email;
+    private long phoneNumber;
+    private int salary;
+    private byte age;
+
+    
+
+}
