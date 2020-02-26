467d1f8aaa3ef3b476a90d6e28b26d88ec7a47b2
dpano <d.panov27@gmail.com>
2020-02-26 22:50:48 +0300

    Person Class and Object 1.2;

--- src/Lesson5/Lesson5Person.java
+++ src/Lesson5/Lesson5Person.java
@@ -1,10 +1,6 @@
 package Lesson5;
 
-import sun.plugin.javascript.navig.Array;
-
-import java.util.Arrays;
-
-class Person {
+class Person{
 
     private String name;
     private String position;
@@ -22,24 +18,48 @@ class Person {
         this.age = age;
     }
 
-    public Person(){
-    }
-
     public void print(){
         System.out.println("Name: " + name + "\nPosition: " + position + "\nEmail: " + email + "\nPhone Number: " + phoneNumber + "\nSalary: " + salary + "\nAge: " + age);
     }
-
-
     public int getAge() {
         return age;
     }
+
+    public Person(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
 
 public class Lesson5Person {
 
     public static void main(String[] args) {
-        Person pers = new Person("IvanovIvan","Engineer", "ivivan@mailbox.com", 892312312, 30000, 30);
-        pers.print();
+//        Person pers = new Person("IvanovIvan","Engineer", "ivivan@mailbox.com", 892312312, 30000, 30);
+//        pers.print();
 
         Person [] persArray = new Person[5];
 
@@ -49,8 +69,9 @@ public class Lesson5Person {
         persArray[3] = new Person("PotapovaOlga","Director", "pot_olga@mailbox.com", 892777799, 150000, 37);
         persArray[4] = new Person("TryshinIgor","Sysadmin", "Tr_igor@mailbox.com", 892345512, 60000, 39);
 
+        System.out.println();
         for (int i = 0; i <persArray.length ; i++) {
-            if (persArray[i].getAge()>=40){
+            if (persArray[i].getAge()>40){
                 persArray[i].print();
             }
         }
